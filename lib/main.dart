29326aa06b252d04f2bd2501776d9a0dba28485d
unknown <danielle.santos@aluno.uece.br>
2022-07-25 11:03:12 -0300

    Fix an api response error in details_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,36 @@
-import 'package:api/pages/details_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'pages/details_page.dart';
 import 'pages/home_page.dart';
+import 'src/models/character.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RMApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class RMApp extends StatelessWidget {
+  const RMApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Kode Start R&M api',
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case HomePage.routeId:
             return MaterialPageRoute(
+              settings: settings,
               builder: (context) {
                 return const HomePage();
               },
-            );    
+            );
           case DetailsPage.routeId:
-            final args = settings.arguments as int;
+            Character detailedCharacter = settings.arguments as Character;
             return MaterialPageRoute(
+              settings: settings,
               builder: (context) {
-                return DetailsPage(
-                  characterId: args,
-                );
+                return DetailsPage(detailedCharacter: detailedCharacter);
               },
             );
           default:
--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,36 +1,27 @@
 import 'package:api/pages/components/app_bar_widget.dart';
-import 'package:api/src/data/get_characters_service.dart';
-import 'package:api/src/theme/app_colors.dart';
+import 'package:api/src/models/character.dart';
 import 'package:flutter/material.dart';
-
-import '../src/models/character.dart';
+import '../src/theme/app_colors.dart';
+import 'components/character_card.dart';
 
 class DetailsPage extends StatefulWidget {
   static const routeId = '/details';
-  final int characterId;
-
-  const DetailsPage({Key? key, required this.characterId}) : super(key: key);
+  final Character detailedCharacter;
+  const DetailsPage({Key? key, required this.detailedCharacter}) : super(key: key);
 
   @override
-  State<DetailsPage> createState() => _DetailsPageState();
+  State<DetailsPage> createState() => _MyHomePageState();
 }
 
-class _DetailsPageState extends State<DetailsPage> {
-  Future<List<Character>>? character;
-
-  @override
-  void initState() {
-    character = CharacterService().getDetailedCharacter(widget.characterId);
-
-    super.initState();
-  }
-
+class _MyHomePageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(
         leftIcon: InkWell(
-          onTap: () => Navigator.pop(context),
+          onTap: () {
+            Navigator.pop(context);
+          },
           child: const Icon(
             Icons.arrow_back,
             color: AppColors.white,
@@ -38,52 +29,9 @@ class _DetailsPageState extends State<DetailsPage> {
         ),
       ),
       backgroundColor: AppColors.backgroundColor,
-      body: FutureBuilder(
-        future: character,
-        builder: (_, AsyncSnapshot<List<Character>> snapshot) {
-          //if (snapshot.hasData) {
-            final data = snapshot.data!;
-
-            return ListView(
-              children: [
-                DetailedCharacterCard(detailedCharacter: data[widget.characterId]),
-              ],
-            );
-          //}
-          //return Container();
-        },
-      ),
-    );
-  }
-}
-
-class DetailedCharacterCard extends StatelessWidget {
-  const DetailedCharacterCard({
-    Key? key,
-    required this.detailedCharacter,
-  }) : super(key: key);
-
-  final Character detailedCharacter;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.primaryLightColor,
-      clipBehavior: Clip.antiAlias,
-      margin:
-          const EdgeInsets.symmetric(horizontal: 20, vertical: 7.5),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadiusDirectional.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
         children: [
-          Image.network(
-            detailedCharacter.image,
-            width: 320,
-            height: 160,
-            fit: BoxFit.fitWidth,
-          ),
+          CharacterCard(character: widget.detailedCharacter),
         ],
       ),
     );
